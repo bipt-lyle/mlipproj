c6443adc314b70dfd0dec07c3a9eee753aa8f546
bipt-lyle <2645385939@qq.com>
2024-02-09 09:26:44 +0100

    yingdaliu1

--- predict.py
+++ predict.py
@@ -11,31 +11,4 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
-    configure_mlflow()
-
-    # 开始MLflow记录
-    with mlflow.start_run():
-        # 执行预测和评估绘图
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        mlflow.log_param("run_date", current_date)
-
-        plots_divs, metrics, predictions_html = evaluate_predict_plots()
-
-        mlflow.log_param("batch_size", 64)
-        mlflow.log_param("epochs", 120)
-        mlflow.log_param("number_of_features", 7)
-        
-        # 记录模型评估指标
-        for key, value in metrics.items():
-            mlflow.log_metric(key, value)
-
-        # 记录预测结果的HTML为工件
-        html_file_path = 'statics/evaluate_predict.html'
-        # 将 HTML 文件记录为工件
-        mlflow.log_artifact(html_file_path)
-
-        predictions_file = "statics/predictions.html"
-        with open(predictions_file, "w") as f:
-            f.write(predictions_html)
-        mlflow.log_artifact(predictions_file)
-        # 可以选择记录其他信息，例如模型参数或配置
+    evaluate_predict_plots()
